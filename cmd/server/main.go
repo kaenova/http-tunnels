210811603f13c4919a4ca964f64b1ace67bb168f
Kaenova PicoClaw <kaenova+picoclaw@gmail.com>
2026-05-31 14:17:05 +0700

    fix: embed admin web assets via go:embed
    - Restore cmd/server/assets.go with //go:embed web/dist
    - Update cmd/server/main.go to pass embedded assets to server
    - Admin panel now served from embedded filesystem

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"io/fs"
 	"log"
 
 	"github.com/kaenova/http-tunnels/internal/server"
 )
 
 func main() {
-	app, err := server.NewApp(server.LoadConfig(), nil)
+	assets, err := fs.Sub(adminAssets, "web/dist")
+	if err != nil {
+		log.Fatalf("loading embedded admin assets failed: %v", err)
+	}
+
+	app, err := server.NewApp(server.LoadConfig(), assets)
 	if err != nil {
 		log.Fatal(err)
 	}
